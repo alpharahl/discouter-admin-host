49fef0cc71d94519e5a0e199d49830591778e4e3
David Gauntlett <david.m.gauntlett@gmail.com>
2020-08-27 21:39:00 -0400

    Deploying docs

---
+++ precache-manifest.1a5098e85a5fd6dc9bb7d9cef393f500.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b596dd84cae8e15e43660403bb978123",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "1f13f70cba5edd3300da",
+    "url": "/css/app.6facd8d9.css"
+  },
+  {
+    "revision": "12b98ddeff08595a1536",
+    "url": "/css/chunk-vendors.2640ef8e.css"
+  },
+  {
+    "revision": "d0f819b2db4c60f1121f5d6d815bacb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f13f70cba5edd3300da",
+    "url": "/js/app.e7fce4f7.js"
+  },
+  {
+    "revision": "12b98ddeff08595a1536",
+    "url": "/js/chunk-vendors.dc41e0d5.js"
+  },
+  {
+    "revision": "e87534c915b991af8ecfee0905099f68",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
